70342ef32912076c5ca96ae0b981e898b5977c7a
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-11-16 16:01:23 -0600

    formatted odds cards

--- src/Odds.jsx
+++ src/Odds.jsx
@@ -1,20 +1,32 @@
+/* eslint-disable react/prop-types */
+import moment from "moment";
 export function Odds(props) {
   console.log(props);
 
+  let currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+
   return (
-    <div id="odds">
-      <h3>Odds</h3>
-      <div className="cards">
+    <div id="odds-index">
+      <h3>Odds!</h3>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
         {props.odds.map((odds) => (
-          <div key={odds.id} className="card">
-            <h2>{odds.sport_title}</h2>
-            <p>Home Team: {odds.bookmakers[0].markets[0].outcomes[0].name}</p>
-            <p>FanDuel line {odds.bookmakers[0].markets[0].outcomes[0].price}</p>
-            <p>Draft Kings line {odds.bookmakers[1].markets[0].outcomes[0].price}</p>
-            <p>Away Team: {odds.bookmakers[0].markets[0].outcomes[1].name}</p>
-            <p>FanDuel line {odds.bookmakers[0].markets[0].outcomes[1].price}</p>
-            <p>Draft Kings line {odds.bookmakers[1].markets[0].outcomes[1].price}</p>
-            <button>Place Bet</button>
+          <div key={odds.id} className="col">
+            <div className="card border-primary mb-3">
+              <h4 className="card-header">{odds.sport_title}</h4>
+              <div className="card-body">
+                <p>{odds.commence_time}</p>
+                <p>{currentDate}</p>
+                <p>Home Team: {odds.bookmakers[0].markets[0].outcomes[0].name}</p>
+                <p>FanDuel line {odds.bookmakers[0].markets[0].outcomes[0].price}</p>
+                <p>Draft Kings line {odds.bookmakers[1].markets[0].outcomes[0].price}</p>
+                <p>Away Team: {odds.bookmakers[0].markets[0].outcomes[1].name}</p>
+                <p>FanDuel line {odds.bookmakers[0].markets[0].outcomes[1].price}</p>
+                <p>Draft Kings line {odds.bookmakers[1].markets[0].outcomes[1].price}</p>
+                <a href="#" className="btn btn-primary">
+                  Place Bet
+                </a>
+              </div>
+            </div>
           </div>
         ))}
       </div>
